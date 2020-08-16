0b72924c8b670fafd96e6ec7126072b73e47494e
Theo Paris <theoparisdesigns@gmail.com>
2020-08-15 22:30:38 -0700

    Create entrypoint.sh

---
+++ root/entrypoint.sh
@@ -0,0 +1,12 @@
+export PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10) && \
+    export SUDO_PASSWORD=$PASSWORD && \
+    echo "Using password: $PASSWORD"
+    
+/usr/bin/code-server \
+			--bind-addr 0.0.0.0:8443 \
+			--user-data-dir /config/data \
+			--extensions-dir /config/extensions \
+			--disable-telemetry \
+			--auth "password" \
+			"${PROXY_DOMAIN_ARG}" \
+			/config/workspace
