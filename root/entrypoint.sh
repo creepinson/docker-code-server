ac088b2b755cfd2ddc02d5046e93de7fd16f6058
Theo Paris <theoparisdesigns@gmail.com>
2020-08-15 22:45:11 -0700

    Update entrypoint.sh

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir -p /config/workspace
 mkdir -p /config/data/logs
+mkdir -p /config/extensions
 
 export PASSWORD=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c10) && \
     export SUDO_PASSWORD=$PASSWORD && \
